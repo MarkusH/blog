f6c7ccb822edb7db93d5c8a5fb27a16f9b531272
Markus Holtermann <info@markusholtermann.eu>
2015-04-21 02:45:46 +0200

    Updated Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,37 +5,22 @@ module.exports = function(grunt) {
       dist: {
         files: [{
           expand: true,
-          cwd: 'bower_components/foundation/js/',
+          cwd: 'materialize/dist/',
           src: [
-            'vendor/jquery.js',
-            'vendor/modernizr.js',
+            'font/**',
           ],
-          dest: 'theme/static/js/',
-          flatten: true,
-          filter: 'isFile',
-        },
-        {
-          expand: true,
-          cwd: 'bower_components/foundation/js/foundation/',
-          src: [
-            'foundation.js',
-            'foundation.clearing.js',
-            'foundation.topbar.js',
-          ],
-          dest: 'theme/static/js/src/',
-          flatten: true,
-          filter: 'isFile',
+          dest: 'theme/static/',
+          flatten: false
         }]
       }
     },
     sass: {
       dist: {
         options: {
-          loadPath: ['bower_components/foundation/scss/'],
+          loadPath: ['materialize/sass/'],
           style: 'compressed'
         },
         files: {
-          'theme/static/css/normalize.css': 'bower_components/foundation/scss/normalize.scss',
           'theme/static/css/main.css': 'theme/static/sass/main.scss'
         }
       }
@@ -45,31 +30,17 @@ module.exports = function(grunt) {
         files: [{
           expand: true,
           flatten: true,
-          cwd: 'theme/static/js',
-          src: 'src/*.js',
+          cwd: 'theme/static/',
+          src: 'js-src/*.js',
           dest: 'theme/static/js'
         }]
       }
     },
     concat: {
-      options: {
-        separator: ';',
-        stripBanners: {block: true},
-        process: function(src, filepath) {
-          if (filepath === 'theme/static/js/jquery.js') {
-            src = src.replace(/\/\*!/g, '\n\/\*!');
-            src = src.replace(/^(\/\*!| \*.*)\n/gm, '');
-          }
-          return src;
-        },
-      },
       dist: {
         src: [
-          'theme/static/js/modernizr.js',
-          'theme/static/js/jquery.js',
-          'theme/static/js/foundation.js',
-          'theme/static/js/foundation.clearing.js',
-          'theme/static/js/foundation.topbar.js',
+          'materialize/dist/js/materialize.min.js',
+          'theme/static/js/init.js',
         ],
         dest: 'theme/static/js/main.js',
       },
