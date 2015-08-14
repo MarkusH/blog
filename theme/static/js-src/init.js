f689b8bec4af4cc90a158439cad2b9d71cfdfb97
Markus Holtermann <info@markusholtermann.eu>
2015-08-15 02:15:12 +1000

    Added search HTML/CSS/JS integration

--- theme/static/js-src/init.js
+++ theme/static/js-src/init.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
   // Mobile navigation
   $('.button-collapse').sideNav();
+  $('.search .button').on('click', function(){$('.search input').focus()})
+
 
   // Masonry layout for index pages
   var $container = $('#posts');
